520240199c03427189224d0bbf0bc679f7407ebb
zoltan.kasza <zoltan.kasza@ibello.hu>
2021-11-03 12:09:17 +0100

    add run mojo

---
+++ src/main/java/hu/ibello/maven/IbelloRun.java
@@ -0,0 +1,27 @@
+package hu.ibello.maven;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
+
+@Mojo(name = "run")
+public class IbelloRun extends IbelloMojo{
+
+    @Parameter(property = "java")
+    private String java;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        runProcess("run");
+    }
+
+    @Override
+    protected List<String> getCalculatedCommand(String command) throws MojoExecutionException{
+        List<String> result = super.getCalculatedCommand(command);
+        appendArgument(result, "--java", java);
+        return result;
+    }
+}
