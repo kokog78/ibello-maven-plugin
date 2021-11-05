8629a7793af07b04e882ecea25da3409a34fefe2
zoltan.kasza <zoltan.kasza@ibello.hu>
2021-11-05 16:37:45 +0100

    update: testmojo

--- src/main/java/hu/ibello/maven/IbelloTestMojo.java
+++ src/main/java/hu/ibello/maven/IbelloTestMojo.java
@@ -1,12 +1,36 @@
 package hu.ibello.maven;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public abstract class IbelloTestMojo extends IbelloMojo{
+
+    @Parameter(defaultValue = "${localRepository}", readonly = true, required = true)
+    private ArtifactRepository local;
+
+    @Component
+    private RepositorySystem repoSystem;
+
+    @Parameter(defaultValue = "${repositorySystemSession}", readonly = true, required = true)
+    private RepositorySystemSession repoSession;
+
+    @Parameter(defaultValue = "${project.remoteProjectRepositories}", readonly = true, required = true)
+    private List<RemoteRepository> repositories;
 
     @Parameter(property = "tags")
     private String[] tags;
@@ -35,7 +59,7 @@ public abstract class IbelloTestMojo extends IbelloMojo{
             }
         }
         appendArgument(result, "--browser", browser);
-        if (size != null) {
+        if (size != null && size.length != 0) {
             if (size.length == 2) {
                 String value = String.format("%dx%d", size[0], size[1]);
                 appendArgument(result, "--size", value);
@@ -47,83 +71,71 @@ public abstract class IbelloTestMojo extends IbelloMojo{
             appendArgument(result, "--repeat", Integer.toString(repeat));
         }
         appendArgument(result, "--pid", getPidFile());
-//        for (File file : getDependencyFiles()) {
-//            appendArgument(result, "--classpath", file.getAbsolutePath());
-//        }
+        getDependencyFiles();
+        for (String file : getDependencyFiles()) {
+            appendArgument(result, "--classpath", file);
+        }
         return result;
     }
 
-    private List<File> getDependencyFiles() {
-//        List<File> result = new ArrayList<>();
-//        List<Configuration> configurations = getConfigurations();
-//        for (Configuration config : configurations) {
-//            Set<File> files = config.getFiles();
-//            if (files != null) {
-//                for (File file : files) {
-//                    if (file.getName().startsWith("ibello")) {
-//                        // skip this file
-//                    } else if (!result.contains(file)) {
-//                        result.add(file);
-//                    }
-//                }
-//            }
-//        }
-//        SourceSet sourceSet = getMainSourceSet();
-//        if (sourceSet != null && sourceSet.getOutput() != null) {
-//            FileCollection files = sourceSet.getOutput().getClassesDirs();
-//            if (files != null) {
-//                for (File dir : files) {
-//                    result.add(dir);
-//                }
-//            }
-//            if (sourceSet.getOutput().getResourcesDir() != null) {
-//                result.add(sourceSet.getOutput().getResourcesDir());
-//            }
-//        }
-//        Collections.sort(result);
-//        return result;
-        return null;
+    private List<String> getDependencyFiles() throws MojoExecutionException {
+        List<String> result = new ArrayList<>();
+        result.add(project.getBuild().getSourceDirectory());
+        Set<Artifact> artifacts = project.getDependencyArtifacts();
+        for (Artifact unresolvedArtifact : artifacts) {
+            String artifactId = unresolvedArtifact.getArtifactId();
+            File file = getResolvedArtifact(unresolvedArtifact);
+            if( file == null || ! file.exists()) {
+                getLog().warn("Artifact " + artifactId + " has no attached file. Its content will not be copied in the target model directory.");
+                continue;
+            }
+            if (isAddable(unresolvedArtifact.getScope())) {
+                result.add(file.getAbsolutePath());
+            }
+        }
+        return result;
     }
 
-//    private List<Configuration> getConfigurations() {
-//        List<Configuration> result = new ArrayList<>();
-//        ConfigurationContainer configurations = getProject().getConfigurations();
-//        if (configurations != null) {
-//            Configuration config = getResolvedConfiguration(configurations, "runtime");
-//            if (config != null) {
-//                result.add(config);
-//            }
-//            config = getResolvedConfiguration(configurations, "runtimeClasspath");
-//            if (config != null) {
-//                result.add(config);
-//            }
-//            config = getResolvedConfiguration(configurations, "implementation");
-//            if (config != null) {
-//                result.add(config);
-//            }
-//            config = getResolvedConfiguration(configurations, "default");
-//            if (config != null) {
-//                result.add(config);
-//            }
-//        }
-//        return result;
-//    }
-//
-//    private Configuration getResolvedConfiguration(ConfigurationContainer configurations, String name) {
-//        Configuration config = configurations.getByName(name);
-//        if (config != null && config.isCanBeResolved()) {
-//            return config;
-//        } else {
-//            return null;
-//        }
-//    }
+    private File getResolvedArtifact (Artifact unresolvedArtifact) throws MojoExecutionException {
+        String artifactId = unresolvedArtifact.getArtifactId();
+        org.eclipse.aether.artifact.Artifact aetherArtifact = new DefaultArtifact(
+                unresolvedArtifact.getGroupId(),
+                unresolvedArtifact.getArtifactId(),
+                unresolvedArtifact.getClassifier(),
+                unresolvedArtifact.getType(),
+                unresolvedArtifact.getVersion());
 
-//    private SourceSet getMainSourceSet() {
-//        JavaPluginConvention java = getProject().getConvention().getPlugin(JavaPluginConvention.class);
-//        if (java != null) {
-//            return java.getSourceSets().getByName("main");
-//        }
-//        return null;
-//    }
+        ArtifactRequest req = new ArtifactRequest().setRepositories(repositories).setArtifact(aetherArtifact);
+        ArtifactResult resolutionResult;
+        try {
+            resolutionResult = repoSystem.resolveArtifact(repoSession, req);
+        } catch(ArtifactResolutionException e) {
+            throw new MojoExecutionException("Artifact " + artifactId + " could not be resolved", e);
+        }
+        return resolutionResult.getArtifact().getFile();
+    }
+
+    private boolean isAddable(String scope) {
+        // TODO melyik scopnál kell?
+        if (scope.equals("compile")) {
+            return true;
+        }
+        if (scope.equals("provided")) {
+            return true;
+        }
+        if (scope.equals("runtime")) {
+            return true;
+        }
+        if (scope.equals("test")) {
+            return false;
+        }
+        if (scope.equals("system")) {
+            return true;
+        }
+        if (scope.equals("import")) {
+            return true;
+        }
+        return false;
+    }
 
 }
